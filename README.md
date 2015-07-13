59dd34d932542c77a6d9553a8605662a4151fd2b
Jack Cherng <jfcherng@gmail.com>
2015-07-13 10:48:13 +0800

    init commit.

---
+++ README.md
@@ -0,0 +1,67 @@
+# TypeShort #
+
+TypeShort is a snippet-like plugin for Sublime Text 3.
+
+It will replace placeholders into corresponding replacements in real-time while typing.
+
+
+## Usage ##
+
+For example, typing a `$` or a `->` in PHP may be just not that comfortable.
+
+You can set `fj_` (`_` means a space here, just for interpretation) as a placeholder for `$` in PHP.
+
+This plugin will automatically replace `fj_` into `$` in PHP whenever you type it.
+
+Although `fj_` has three characters, it could still be typed faster than a `$`.
+
+
+## Installation ##
+
+Install using Package Control (Recommended), or by cloning this repository into the Packages directory.
+
+Note that this plugin need you to set your own (`placeholder`, `replacement`) pairs to work properly.
+
+
+## Settings ##
+
+Edit settings from the menu `Preferences » Package Settings » TypeShort » Settings - User`.
+
+Settings structure:
+```json
+{
+    "bindings": [
+        {
+            "keymaps": {
+                "placeholder_1": "replacement_1",
+                "placeholder_2": "replacement_2"
+                ...
+            },
+            "syntax_list": ["syntax_1", "syntax_2", ...]
+        },
+        ...
+    ]
+}
+```
+
+Example settings:
+```json
+{
+    "bindings": [
+        {
+            "keymaps": {
+                "fj ": "$",
+                "dk ": "->"
+            },
+            "syntax_list": ["PHP"]
+        },
+        {
+            "keymaps": {
+                "fj ": "*",
+                "dk ": "->"
+            },
+            "syntax_list": ["C", "C++", "C++11", "C#", "Objective-C", "Objective-C++"]
+        }
+    ]
+}
+```
---
+++ TypeShort.py
@@ -0,0 +1,90 @@
+"""
+typeShort is a free software under MIT license.
+This plugin allows you to set shortcuts for certain strings.
+
+Copyright (c) 2015, jfcherng.
+All rights reserved.
+"""
+
+
+import os
+import sublime
+import sublime_plugin
+
+
+def Singleton(class_):
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
+
+
+@Singleton
+class typeShort():
+
+    def __init__(self):
+        self.SETTINGS_FILE_NAME = "TypeShort.sublime-settings"
+
+        self.plugin_settings = sublime.load_settings(self.SETTINGS_FILE_NAME)
+        self.bindings = self.plugin_settings.get("bindings", None)
+
+    def do_replace(self, view, binding, lastInsertedChar):
+        for search, replacement in binding['keymaps'].items():
+            # skip a keymap as early as possible
+            if not lastInsertedChar == search[-1]:
+                continue
+            # iterate each selection
+            for editRegion in view.sel():
+                checkRegion = sublime.Region(
+                    editRegion.begin() - len(search),
+                    editRegion.end()
+                )
+                if view.substr(checkRegion) == search:
+                    view.run_command("type_short", {
+                        "begin": checkRegion.begin(),
+                        "end": checkRegion.end(),
+                        "replacement": replacement
+                    })
+                    return True
+        return False
+
+
+class typeShortCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, begin, end, replacement):
+        self.view.replace(edit, sublime.Region(begin, end), replacement)
+
+
+class typeShortListener(sublime_plugin.EventListener):
+
+    def get_syntax(self, view):
+        syntax = os.path.basename(view.settings().get("syntax"))
+        syntax = os.path.splitext(syntax)[0]
+        return syntax
+
+    def on_modified(self, view):
+        # Fix the issue that breaks functionality for Ctrl+Z
+        historyCmd = view.command_history(1)
+        if historyCmd[0] == 'type_short':
+            return
+
+        # Get the last press key
+        historyCmd = view.command_history(0)
+        if not historyCmd[0] == 'insert':
+            return
+        lastInsertedChar = historyCmd[1]['characters']
+
+        obj = typeShort()
+        currentSyntax = self.get_syntax(view)
+        for binding in obj.bindings:
+            if currentSyntax in binding['syntax_list']:
+                replaced = obj.do_replace(view, binding, lastInsertedChar)
+                if replaced is True:
+                    break
+
+
+def plugin_loaded():
+    typeShort()
