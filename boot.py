9227fb8a1a9d4ead93a1f90f715defad18eb8ae1
Jack Cherng <jfcherng@gmail.com>
2019-09-02 07:02:38 +0800

    Make boot.py cleaner
    And also we can applied mypy on plugin/__init__.py
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- boot.py
+++ boot.py
@@ -1,6 +1,4 @@
-from .plugin.BindingsCompiler import BindingsCompiler
-from .plugin.Globals import Globals
-from .plugin.settings import get_setting, get_settings_object, get_settings_file
+from .plugin import set_up, tear_down
 
 # main plugin classes
 from .plugin.TypeShort import *
@@ -8,15 +6,8 @@ from .plugin.TypeShortCommand import *
 
 
 def plugin_loaded() -> None:
-    def plugin_settings_listener() -> None:
-        """ called when the settings file is changed """
-
-        Globals.bindings = BindingsCompiler(get_setting("bindings")).compile()
-
-    # when the user settings is modified...
-    get_settings_object().add_on_change(get_settings_file(), plugin_settings_listener)
-    plugin_settings_listener()
+    set_up()
 
 
 def plugin_unloaded() -> None:
-    get_settings_object().clear_on_change(get_settings_file())
+    tear_down()
---
+++ plugin/__init__.py
@@ -0,0 +1,22 @@
+from .BindingsCompiler import BindingsCompiler
+from .Globals import Globals
+from .settings import get_setting, get_settings_object, get_settings_file
+
+
+def set_up() -> None:
+    """ plugin_loaded """
+
+    def plugin_settings_listener() -> None:
+        """ called when the settings file is changed """
+
+        Globals.bindings = BindingsCompiler(get_setting("bindings")).compile()
+
+    # when the user settings is modified...
+    get_settings_object().add_on_change(get_settings_file(), plugin_settings_listener)
+    plugin_settings_listener()
+
+
+def tear_down() -> None:
+    """ plugin_unloaded """
+
+    get_settings_object().clear_on_change(get_settings_file())
