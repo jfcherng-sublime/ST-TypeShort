6bfd0f84887c21d88010ff8e90794527ac1183f6
Jack Cherng <jfcherng@gmail.com>
2022-10-02 07:49:50 +0800

    chore: update ST stubs (#6)

--- boot.py
+++ boot.py
@@ -1,8 +1,8 @@
 from .plugin import set_up, tear_down
 
 # main plugin classes
-from .plugin.sublime_text.TypeShort import *
-from .plugin.sublime_text.TypeShortCommand import *
+from .plugin.sublime_text.TypeShort import *  # noqa: F401, F403
+from .plugin.sublime_text.TypeShortCommand import *  # noqa: F401, F403
 
 
 def plugin_loaded() -> None:
